da95de7cb91d83f795f9081a0b234eabf2e32038
Toni G <toni.giorgino@gmail.com>
2013-10-24 09:59:29 +0200

    tested parameters on warfarin

--- charmm_utilities/all-parameters/make.sh
+++ charmm_utilities/all-parameters/make.sh
@@ -1,28 +1,44 @@
+#!/bin/bash
+
 # Combine well-known CHARMM parameter files into one
 
+function ech () {
+	echo -e "\n\n\n*********** $1 \n\n\n"
+}
 
 # All files contained in this archive from 
 # http://mackerell.umaryland.edu/CHARMM_ff_params.html
 # File: toppar_c36_aug12.tgz
 
-echo "1. Getting toppar_c36_aug12.tgz from http://mackerell.umaryland.edu..."
 
-wget -O- 'http://mackerell.umaryland.edu/download.php?filename=CHARMM_ff_params_files/toppar_c36_aug12.tgz' | tar -zxf-
+c36=toppar_c36_aug12.tgz
+ech "1. Getting $c36 from http://mackerell.umaryland.edu..."
+
+if [ ! -f $c36 ]; then
+	wget 'http://mackerell.umaryland.edu/download.php?filename=CHARMM_ff_params_files/toppar_c36_aug12.tgz' -O $c36
+else
+	echo "Already there"
+fi
+
+tar -zxvf $c36
 
 
 # Split the water stream and fix leading asterisks
-echo "2. Splitting water stream"
+ech "2. Splitting water stream"
 ../split_stream.pl toppar/toppar_water_ions.str
 sed -i 's/^\*/!/g' par_water_ions.prm;
 
-echo "3. Combining parameters"
-../combine_parameters.pl -f toppar/par_all36_prot.prm \
-	toppar/par_all35_ethers.prm \
-	toppar/par_all36_carb.prm \
+
+# Combine
+ech "3. Combining parameters"
+../combine_parameters.pl -f \
+	toppar/par_all36_prot.prm \
 	toppar/par_all36_cgenff.prm \
 	toppar/par_all36_lipid.prm \
 	toppar/par_all36_na.prm \
-	toppar/par_water_ions.prm \
+	toppar/par_all35_ethers.prm \
+	toppar/par_all36_carb.prm \
+	par_water_ions.prm \
 		> par_all36_prot22_na_lipid_carb_ethers_cgenff_water_ions.prm
 
 
@@ -32,16 +48,17 @@ echo "3. Combining parameters"
 # (which includes water and ions)
 # 108b83d5b5e33e13bd9ecd0592c45c60  par_all22star_prot_revision-one.inp
 
-echo "4. Getting Charmm22* revision one from home.uchicago.edu/~/kippjohnson"
+ech "4. Getting Charmm22* revision one from home.uchicago.edu/~/kippjohnson"
+rm par_all22star_prot_revision-one.inp
 wget http://home.uchicago.edu/~/kippjohnson/par_all22star_prot_revision-one.inp
 
-echo "5. Merging parameters"
+ech "5. Merging parameters"
 ../combine_parameters.pl -f par_all22star_prot.inp \
-	par_all35_ethers.prm \
-	par_all36_carb.prm \
-	par_all36_cgenff.prm \
-	par_all36_lipid.prm \
-	par_all36_na.prm \
+	toppar/par_all36_cgenff.prm \
+	toppar/par_all36_lipid.prm \
+	toppar/par_all36_na.prm \
+	toppar/par_all35_ethers.prm \
+	toppar/par_all36_carb.prm \
 		> par_all36_prot22star_na_lipid_carb_ethers_cgenff_water_ions.prm
 
 
